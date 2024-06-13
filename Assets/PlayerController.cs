1edf48ebb180cdd44a4019f20e57b3ff8e4ae1f5
ikkiren <chris_dupuis@outlook.com>
2024-06-13 17:49:11 +0200

    yes

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
 {
 
     Rigidbody rb;
+    public GameObject sceneCamera;
+    public int speed = 1;
+    public float jumpForce = 150f;
     int jump = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +18,10 @@ public class PlayerController : MonoBehaviour
     }
 
     void OnCollisionEnter(Collision collision) {
-        if (collision.collider.name == "Floor")
+        if (collision.collider.name == "Floor") {
             Debug.Log("Game Over");
+            Destroy(gameObject);
+        }
         jump = 0;
     }
 
@@ -30,12 +36,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        sceneCamera.transform.LookAt(gameObject.transform);
+        sceneCamera.transform.position = new Vector3(sceneCamera.transform.position.x, sceneCamera.transform.position.y, gameObject.transform.position.z - 4);
+
         float xVel = Input.GetAxis("Horizontal");
         float zVel = Input.GetAxis("Vertical");
+
         if (jump == 0 && Input.GetKeyDown("space")) {
-            rb.AddForce(new Vector3(0, 150f, 0));
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            rb.AddForce(new Vector3(0, jumpForce, 0));
             jump = 1;
         }
-        rb.velocity = new Vector3(xVel, rb.velocity.y, zVel) * 1;
+        rb.velocity = new Vector3(xVel, rb.velocity.y, zVel) * speed;
     }
 }
